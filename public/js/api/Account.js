1fb86b520d4ff75d29dd5c26a6b61de055336f33
Oksana Danilova <ilimlaw38@gmail.com>
2024-10-04 19:49:52 +0800

    initial commit

--- public/js/api/Account.js
+++ public/js/api/Account.js
@@ -4,10 +4,15 @@
  * Имеет свойство URL со значением '/account'
  * */
 class Account extends Entity {
-  /**
-   * Получает информацию о счёте
-   * */
-  static get(id = '', callback){
-
-  }
+	static URL = '/account';
+	/**
+	 * Получает информацию о счёте
+	 * */
+	static get(id = '', callback) {
+		createRequest({
+			url: this.URL + '/' + id,
+			method: 'GET',
+			callback,
+		});
+	}
 }
