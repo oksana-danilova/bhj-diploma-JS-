f657ad6e37ee38b31e07f9f103c6d07643790fa2
Oksana Danilova <ilimlaw38@gmail.com>
2024-10-04 19:53:19 +0800

    initial commit

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,40 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+	const xhr = new XMLHttpRequest();
+	xhr.responseType = 'json';
 
-};
+	let {
+		url,
+		method,
+		data
+	} = options;
+	let formData = new FormData();
+
+	if (options.data) {
+		data = Object.entries(options.data);
+
+		if (method === 'GET') {
+			for (let [key, value] of data) {
+				url = url + '?' + key + '=' + value;
+			}
+		}
+
+		if (method !== 'GET') {
+			for (let [key, value] of data) {
+				formData.append(key, value);
+			}
+		}
+	}
+
+	xhr.addEventListener('load', () => {
+		options.callback(null, xhr.response);
+	});
+
+	try {
+		xhr.open(method, url);
+		xhr.send(formData);
+	} catch (err) {
+		options.callback(err, xhr.response);
+	}
+}
