65e92793b9d1de09641539cd630e765d64a75382
Oksana Danilova <ilimlaw38@gmail.com>
2024-10-07 18:33:42 +0800

    initial commit

--- public/js/ui/forms/LoginForm.js
+++ public/js/ui/forms/LoginForm.js
@@ -10,6 +10,16 @@ class LoginForm extends AsyncForm {
    * закрывает окно, в котором находится форма
    * */
   onSubmit(data) {
-
+    try {
+      const response = User.login(data);
+      if (response && response.error) {
+        throw new Error(response.error);
+      }
+      App.setState('user-logged');
+      this.element.reset();
+      this.element.closest('.modal').style.display = 'none';
+    } catch (error) {
+      console.error('Ошибка при авторизации:', error.message);
+    }
   }
 }
