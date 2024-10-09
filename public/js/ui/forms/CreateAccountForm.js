163af3194bac4d0bb328b8cbbf91f5aa392aee7c
Oksana Danilova <ilimlaw38@gmail.com>
2024-10-09 18:28:12 +0800

    initial commit

--- public/js/ui/forms/CreateAccountForm.js
+++ public/js/ui/forms/CreateAccountForm.js
@@ -9,6 +9,12 @@ class CreateAccountForm extends AsyncForm {
    * и сбрасывает форму
    * */
   onSubmit(data) {
-
+    Account.create(data, (response) => {
+      if (response && response.success) {
+        App.update();
+        this.element.reset();
+        App.getModal("createAccount").close();
+      }
+    });
   }
 }
