999ce75a7479f3dc85c7d403c8c0ecacf9cc0fac
Oksana Danilova <ilimlaw38@gmail.com>
2024-10-19 11:35:55 +0800

    исправлен callback

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,43 +3,30 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
-	const xhr = new XMLHttpRequest();
-	xhr.responseType = 'json';
-
-	let {url, method, data, callback} = options;
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+	let { url, method, data } = options;
 	let formData = new FormData();
-
-	if (data) {
-		data = Object.entries(options.data);
-
-		if (method === 'GET') {
-			for (let [key, value] of data) {
-				url = url + '?' + key + '=' + value;
-			}
-		} else {
-			for (let [key, value] of data) {
-				formData.append(key, value);
-			}
+  
+	if (options.data) {
+	  data = Object.entries(options.data);
+  
+	  if (method === "GET") {
+		for (let [key, value] of data) {
+		  url = url + "?" + key + "=" + value;
 		}
-		/* 
-		if (method === 'GET') {
-			url += '?' + [...data].map(([key, value]) => `${key}=${value}`).join('&');
-		} else {
-			for (let [key, value] of data) {
-				formData.append(key, value);
-			}
+	  } else {
+		for (let [key, value] of data) {
+		  formData.append(key, value);
 		}
-		*/
+	  }
 	}
 
-	xhr.addEventListener('load', () => {
-		callback(null, data);
-	});
+    xhr.onload = () => {
+        options.callback(null, xhr.response);
+    };
 
-	try {
-		xhr.open(method, url);
-		xhr.send(formData);
-	} catch (err) {
-		callback(err, data);
-	}
-}
+    xhr.open(options.method, url);
+
+    xhr.send(options.method === 'GET' ? null : formData);
+};
